b102a8e546b782d5ffd2d37126886d82e79748ae
mamalisantra <108969855+mamalisantra@users.noreply.github.com>
2025-05-07 11:41:02 +0530

    created SampleForm

--- src/Components.js/About.js
+++ src/Components.js/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="container-fluid d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
     <div className="text-center text-success h5 p-5 bg-light rounded shadow-lg">
-      <h1>Welcome to About Page !!!!!!!!</h1>
+      <h1>Welcome to About Page !!!!!!!!!</h1>
       <p className="mt-3">Enjoy your time exploring!</p>
     </div>
   </div>
---
+++ src/Components.js/SampleForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SampleForm = () => {
+  return (
+    <div>SampleForm</div>
+  )
+}
+
+export default SampleForm
