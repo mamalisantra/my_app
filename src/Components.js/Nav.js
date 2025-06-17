67fe080fdb33f1d55d8d5aedd1e3c587eebc733a
mamali <mamalisantra@gmail.com>
2025-06-17 16:05:39 +0530

    Nav.js

--- src/Components.js/Nav.js
+++ src/Components.js/Nav.js
@@ -5,14 +5,14 @@ const Nav = () => {
     return (
         <>
             <nav className='navbar'>
-                <ul className="ul">This is Navbar
+                <ul className="ul">navbar local
                     <li className="li" onClick={() => navigate('/home')}>Home</li>
                     <li className="li" onClick={() => navigate('/about')}>About</li>
                     <li className="li" onClick={() => navigate('/sampleReactTable')}>Get Data</li>
                     <li className="li" onClick={() => navigate('/sampleFields')}>Get Data</li>
                 </ul>
             </nav>
-            
+
         </>
     );
 };
